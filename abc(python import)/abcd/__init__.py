a2c75ab254da527bf5b604cf1695a57e690dc349
ijpq <tangke0320@hotmail.com>
2019-12-13 17:41:29 +0800

    x

---
+++ abc(python import)/abcd/__init__.py
@@ -0,0 +1,1 @@
+from .classa import A
---
+++ abc(python import)/abcde/classb.py
@@ -0,0 +1,14 @@
+# from __future__ import absolute_import
+import sys
+sys.path.append('..')
+print(sys.path)
+
+from abcd.classa import A
+
+
+# a=A()
+
+# class B():
+#     def __init__(self):
+#         print('B')
+
