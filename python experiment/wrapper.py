bf0d546603e0a88cc5aeb24f1175a67fbcf3ae24
ijpq <tangke0320@hotmail.com>
2019-12-21 15:23:17 +0800

    wrapper

--- python experiment/wrapper.py
+++ python experiment/wrapper.py
@@ -1,6 +1,8 @@
 def log(func):
     def wrapper(*args,**kwargs):
         print('before wrapper')
+        print('args', args)
+        print('kwargs', kwargs)
         return func(*args,**kwargs)
     return wrapper
 
@@ -12,10 +14,11 @@ class CLS():
     
     @log # eqs under_wrapper = log(under_wrapper)
     def under_wrapper(self,*args,**kwargs):
-        print('args',args)
-        print('kwargs',kwargs)
+        
         print(self.__class__.__name__)
 
 f=CLS()
+f.under_wrapper()
+#under_wrapper = log(under_wrapper)
 f.under_wrapper(1,2,3)
 #under_wrapper(1,2,3) = log(under_wrapper)(1,2,3)
